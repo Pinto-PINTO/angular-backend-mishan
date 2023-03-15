624d8282a4abe703c7da05d14701a7ad139cbff7
Menuka Pinto <instafeed9999@gmail.com>
2023-03-16 00:48:53 +0530

    Created Employee without reactive-forms

--- EmployeeUI/src/app/employee/add-edit-emp/add-edit-emp.component.ts
+++ EmployeeUI/src/app/employee/add-edit-emp/add-edit-emp.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {SharedService} from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-edit-emp',
@@ -6,6 +7,57 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class AddEditEmpComponent {
+export class AddEditEmpComponent implements OnInit{
+
+  constructor(private service:SharedService) {}
+
+  // List to store all departments
+  EmployeeList:any=[];
+
+  @Input() emp:any;
+  EmployeeId:any;
+  EmployeeName:any;
+  Department:any;
+  DateOfJoining:any;
+  PhotoFileName:any;
+  
+  ngOnInit(): void {
+    this.EmployeeId=this.emp.EmployeeId;
+    this.EmployeeName=this.emp.EmployeeName;
+    this.Department=this.emp.Department;
+    this.DateOfJoining=this.emp.DateOfJoining;
+    this.PhotoFileName=this.emp.PhotoFileName;
+    this.refreshEmpList();
+  }
+
+  addEmployee(){
+    var val = {EmployeeId:this.EmployeeId,
+      EmployeeName:this.EmployeeName,
+      Department:this.Department,
+      DateOfJoining:this.DateOfJoining,
+      PhotoFileName:this.PhotoFileName,
+    };
+    this.service.addEmployee(val).subscribe(res=>{
+      alert(res.toString());
+    });
+  }
+
+  updateEmployee(){
+    var val = {EmployeeId:this.EmployeeId,
+      EmployeeName:this.EmployeeName,
+      Department:this.Department,
+      DateOfJoining:this.DateOfJoining,
+      PhotoFileName:this.PhotoFileName,
+    };
+    this.service.updateEmployee(val).subscribe(res=>{
+    alert(res.toString());
+    });
+  }
+
+  refreshEmpList(){
+    this.service.getEmpList().subscribe(data=>{
+      this.EmployeeList=data;
+    });
+  }
 
 }
--- EmployeeUI/src/app/employee/show-delete-emp/show-delete-emp.component.ts
+++ EmployeeUI/src/app/employee/show-delete-emp/show-delete-emp.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {SharedService} from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-delete-emp',
@@ -6,6 +7,60 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ShowDeleteEmpComponent {
+export class ShowDeleteEmpComponent implements OnInit{
 
+  constructor(private service:SharedService) { }
+
+  // List to store all employees
+  EmployeeList:any=[];
+
+  // Varibales in Modal
+  Title:any;
+  ActivateAddEditEmpComp:boolean=false;
+  emp:any;
+
+
+  ngOnInit(): void {
+    this.refreshEmpList();
+  }
+
+  openModal() {
+    this.Title="Add Employee";
+    this.emp={
+      EmployeeId:0,
+      EmployeeName:'',
+      Department:'',
+      DateOfJoining:'',
+      PhotoFileName:'',
+    }
+    this.ActivateAddEditEmpComp=true;
+  }
+
+  editClick(item:any) {
+    this.Title="Edit Employee";
+    this.emp=item;
+    this.ActivateAddEditEmpComp=true;  
+  }
+
+  deleteClick(item:any) {
+    if(confirm("Are you sure you want to delete ?")){
+      this.service.deleteDepartment(item.DepartmentId).subscribe(data=>{
+        this.refreshEmpList();
+        alert(data.toString());
+      })
+    }
+  }
+
+
+  closeModalClick() {
+    this.ActivateAddEditEmpComp=false;
+    this.refreshEmpList();
+  }
+
+
+  refreshEmpList(){
+    this.service.getEmpList().subscribe(data=>{
+      this.EmployeeList=data;
+    });
+  }
 }
